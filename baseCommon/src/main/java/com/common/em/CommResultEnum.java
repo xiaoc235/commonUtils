4bfcc29b5e9d137f4df81bc05f490cb25bf00816
jianghaoming <xiaoc235@163.com>
2018-04-09 15:25:30 +0800

    增加 ret int 方法

--- baseCommon/src/main/java/com/common/em/CommResultEnum.java
+++ baseCommon/src/main/java/com/common/em/CommResultEnum.java
@@ -1,8 +1,5 @@
 package com.common.em;
 
-import java.util.HashMap;
-import java.util.Map;
-
 /**
 *  0:否 1:是
  */
@@ -12,30 +9,17 @@ public enum CommResultEnum {
 
     private String value ;
 
-    private static Map<String, String> desc = new HashMap<String, String>();
-    static {
-        desc.put(YES.getValue(),"是");
-        desc.put(NO.getValue(),"否");
-    }
-
     private CommResultEnum(final String _value) {
         this.value = _value;
     }
-    public static Map<String,String> getAllMap(){
-        Map<String,String> resultMap = new HashMap<String,String>();
-        resultMap.putAll(desc);
-        return resultMap;
-    }
+
     public String getValue() {
         return value;
     }
 
-    public static String getDesc(final String key) {
-        if(desc.containsKey(key)){
-            return desc.get(key);
-        }else{
-            return "";
-        }
+    public Integer getIntValue(){
+        return Integer.parseInt(value);
     }
+
 
 }
