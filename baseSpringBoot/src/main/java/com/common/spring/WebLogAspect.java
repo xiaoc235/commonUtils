7d35babaf240c9d46567e96c8b8c4ec69542f31f
jianghaoming <xiaoc235@163.com>
2018-05-07 15:22:39 +0800

    getContentType  eqls ==> getContentType contains

--- baseSpringBoot/src/main/java/com/common/spring/WebLogAspect.java
+++ baseSpringBoot/src/main/java/com/common/spring/WebLogAspect.java
@@ -92,7 +92,7 @@ public class WebLogAspect {
         requestStr.append(WRAN_LINE_SIGN);
         if(joinPoint.getArgs()!=null) {
             requestStr.append("Body args : ");
-            if(applicatoin_json.equals(request.getContentType())) {
+            if(applicatoin_json.contains(request.getContentType())) {
                 requestStr.append(GsonUtils.toJson(joinPoint.getArgs()));
             }else{
                 for (int i = 0; i < joinPoint.getArgs().length; i++) {
