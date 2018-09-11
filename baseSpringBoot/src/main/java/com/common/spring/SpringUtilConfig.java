4abf9cf53377a38810d350eae6968ff3b6a6fce9
jianghaoming <xiaoc235@163.com>
2018-09-11 16:44:03 +0800

    拆分spring相关模块 --》 baseSpring

--- baseSpringBoot/src/main/java/com/common/spring/SpringUtilConfig.java
+++
@@ -1,9 +0,0 @@
-package com.common.spring;
-
-import org.springframework.context.annotation.ComponentScan;
-
-
-@ComponentScan(basePackages={"com.common.dao","com.common.redis","com.common.spring"})
-public class SpringUtilConfig {
-
-}
